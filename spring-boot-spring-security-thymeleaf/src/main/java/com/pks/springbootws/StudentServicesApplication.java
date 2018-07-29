9078235d33baa48ebd00679dceaa82938808fcdd
Prasanta Kumar Sahoo <sahoop092@gmail.com>
2018-07-29 17:39:09 +0530

    Spring Security Project

---
+++ spring-boot-spring-security-thymeleaf/src/main/java/com/pks/springbootws/StudentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.pks.springbootws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentServicesApplication.class, args);
+	}
+	
+}
